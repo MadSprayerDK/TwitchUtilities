e7695a24d9535859da9044ff4c6ef47bb28b86b3
Martin Manø <mail@m-mano.dk>
2015-05-28 11:17:04 +0200

    ADD - Twitch Api Project WIP - Basic functionallity

---
+++ Twitch.Api/TwitchApi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Twitch.Api.Model;
+
+namespace Twitch.Api
+{
+    public class TwitchApi
+    {
+        public string Channel { set; get; }
+        private string ApiBaseUri = "https://api.twitch.tv/kraken/";
+
+        public TwitchApi()
+        { }
+
+        public TwitchApi(string channel)
+        {
+            Channel = channel;
+        }
+
+        public Channel GetChannel()
+        {
+            return JsonConvert.DeserializeObject<Channel>(PerformHttpsRequest("channels/" + Channel));
+        }
+
+        public Stream GetStream()
+        {
+            return JsonConvert.DeserializeObject<Stream>(PerformHttpsRequest("streams/" + Channel));
+        }
+
+        private string PerformHttpsRequest(string uri)
+        {
+            if(string.IsNullOrEmpty(Channel))
+                throw new Exception("No channel is set");
+
+            return "";
+        }
+    }
+}
--- TwitchUtilities/MainWindow.xaml.cs
+++ TwitchUtilities/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +25,45 @@ namespace TwitchUtilities
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void UpdaetLabelBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText("Labels/CustomLabel.txt", CustomLabel.Text);
+        }
+
+        private void StartCountdown_OnClick(object sender, RoutedEventArgs e)
+        {
+            double hours;
+            double minutes;
+            double seconds;
+
+            double.TryParse(CountdownHours.Text, out hours);
+            double.TryParse(CountdownMinutes.Text, out minutes);
+            double.TryParse(CountdownSeconds.Text, out seconds);
+
+
+            var targetDateTime = DateTime.Now.
+                AddHours(hours)
+                .AddMinutes(minutes)
+                .AddSeconds(seconds);
+
+            var timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += (o, args) =>
+            {
+                var difference = targetDateTime - DateTime.Now;
+
+                if(Math.Abs(difference.TotalSeconds) < 1)
+                    timer.Stop();
+                
+                var output = difference.ToString(@"hh\:mm\:ss");
+                CountdownOutputLabel.Dispatcher.BeginInvoke((Action)(() => CountdownOutputLabel.Content = output));
+            };
+            timer.AutoReset = true;
+            timer.Start();
+
+            //throw new NotImplementedException();
         }
     }
 }
