2e84832fb44762160eaa934a1fd9a8e7afad5359
Martin Manø <mail@m-mano.dk>
2015-06-16 02:56:41 +0200

    ADD - Ability to use variables in Custom Label

--- TwitchUtilities/UserInterface/Labels.xaml.cs
+++ TwitchUtilities/UserInterface/Labels.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
+using TwitchUtilities.UserInterface.LabelsPanels;
 
 namespace TwitchUtilities.UserInterface
 {
@@ -14,11 +18,14 @@ namespace TwitchUtilities.UserInterface
         private TimeSpan _counting;
         private readonly Timer _countdownTimer;
 
+        private readonly Dictionary<string, string> _customLabelCache;
+
         public Labels()
         {
             InitializeComponent();
 
             CountdownTextInput.Text = "You can use the {time} identifier here";
+            _customLabelCache = new Dictionary<string, string>();
 
             _countdownTimer = new Timer
             {
@@ -30,7 +37,62 @@ namespace TwitchUtilities.UserInterface
 
         private void UpdateLabelBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            File.WriteAllText("Labels/CustomLabel.txt", CustomLabel.Text);
+            File.WriteAllText("Labels/CustomLabel.txt", GetCustomLabelContent());
+        }
+
+        private void CustomLabel_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var activeKeys = new List<string>();
+            var matches = Regex.Matches(CustomLabel.Text, @"\{(\s*?.*?)\}");
+
+            foreach (Match match in matches)
+            {
+                var key = match.Value.Substring(1, match.Value.Length - 2);
+
+                if (!_customLabelCache.ContainsKey(key))
+                    _customLabelCache.Add(key, "");
+
+                activeKeys.Add(key);
+            }
+
+            CustomVariables.Children.Clear();
+
+            foreach (var key in activeKeys)
+            {
+                var newVariable = new CustomLabelVariable(key, _customLabelCache[key]);
+                newVariable.InputChanged += CustomLabelVariableChanged;
+                CustomVariables.Children.Add(newVariable);
+            }
+
+            if (activeKeys.Any())
+            {
+                var text = GetCustomLabelContent();
+                CustomLabelPreview.Content = text.Length == 0 ? "<Label output preview>" : text;
+                CustomLabelPreview.Visibility = Visibility.Visible;
+            }
+            else
+                CustomLabelPreview.Visibility = Visibility.Collapsed;
+        }
+
+        private void CustomLabelVariableChanged(object sender, EventArgs args)
+        {
+            var sendingVariable = (CustomLabelVariable) sender;
+            _customLabelCache[sendingVariable.Title] = sendingVariable.Value;
+
+            CustomLabelPreview.Content = GetCustomLabelContent();
+        }
+
+        private string GetCustomLabelContent()
+        {
+            var resultString = CustomLabel.Text;
+
+            foreach (var variable in _customLabelCache)
+            {
+                if (resultString.IndexOf("{" + variable.Key + "}", StringComparison.Ordinal) != -1)
+                    resultString = resultString.Replace("{" + variable.Key + "}", variable.Value);
+            }
+
+            return resultString;
         }
 
         private void ToggleCountdown_OnClick(object sender, RoutedEventArgs e)
---
+++ TwitchUtilities/UserInterface/LabelsPanels/CustomLabelVariable.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace TwitchUtilities.UserInterface.LabelsPanels
+{
+    /// <summary>
+    /// Interaction logic for CustomLabelVariable.xaml
+    /// </summary>
+    public partial class CustomLabelVariable
+    {
+        public delegate void InputChangedEventHandler(object sender, EventArgs e);
+        public event InputChangedEventHandler InputChanged;
+
+        public string Title
+        {
+            set { VariableTitle.Content = value; }
+            get { return VariableTitle.Content.ToString(); }
+        }
+
+        public string Value
+        {
+            set { VariableText.Text = value; } 
+            get { return VariableText.Text; }
+        }
+
+        public CustomLabelVariable()
+        {
+            InitializeComponent();
+        }
+
+        public CustomLabelVariable(string title, string value)
+        {
+            InitializeComponent();
+            Title = title;
+            Value = value;
+        }
+
+        private void VariableText_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (InputChanged != null)
+                InputChanged(this, e);
+        }
+    }
+}
